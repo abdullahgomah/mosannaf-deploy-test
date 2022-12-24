08e24027c22fe5f2502f4f1c91bbf830783488cd
AMStudioAM <abdullahgomah2000@gmail.com>
2022-12-24 03:02:39 +0300

    add csrf settings

--- project/settings.py
+++ project/settings.py
@@ -152,3 +152,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-b14c.up.railway.app']
